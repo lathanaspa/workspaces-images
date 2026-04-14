b949d47c19f2b6d4137ff399137f448f0a8f2d92
bbincwebmaster <webmaster@blueblossominc.com>
2026-04-14 02:40:05 -0500

    Add script to restrict to single app

---
+++ src/ubuntu/install/misc/restrict_to_single_app.sh
@@ -0,0 +1,4 @@
+# Update the desktop environment to be optimized for a single application
+RUN cp $HOME/.config/xfce4/xfconf/single-application-xfce-perchannel-xml/* $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/
+RUN cp /usr/share/backgrounds/bg_kasm.png /usr/share/backgrounds/bg_default.png
+RUN apt-get remove -y xfce4-panel
